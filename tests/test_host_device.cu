6a7fcea3172251141e54ed56a0f627b9aad5d305
Fangzhou Ai <fangzhouai@gmail.com>
2023-10-08 20:41:03 -0700

    Add OpenMP test

--- tests/test_host_device.cu
+++ tests/test_host_device.cu
@@ -1,9 +1,25 @@
 #include "../include/puff.h"
+#include <gtest/gtest.h>
+#include <omp.h>
 
-int main()
+TEST(PUFF, init)
 {
 #ifdef USE_OPENMP
     omp_set_num_threads(omp_get_max_threads());
+    std::cout << "**********GTest initialized with OpenMP**********" << std::endl;
+#else
+    std::cout << "**********GTest initialized without OpenMP**********" << std::endl;
 #endif
-    return 0;
+}
+
+TEST(PUFF, Check_OMP)
+{
+#ifdef USE_OPENMP
+#pragma omp parallel
+    {
+        EXPECT_EQ(omp_get_num_threads(), omp_get_max_threads());
+    }  
+#else
+    ASSERT_TRUE(true);
+#endif
 }
